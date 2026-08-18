417e29be090616638c5645bf132cbfc5ebf6fdd2
Allan Storch Garcia <allan.storch21@gmail.com>
2026-08-18 17:03:04 -0300

    Add files via upload

---
+++ aulas/Aula 02/ProjetoCalculadora/src/main/java/com/mycompany/calculadora/ProjetoCalculadora.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author MorraMicrosoft
+ */
+public class ProjetoCalculadora {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
